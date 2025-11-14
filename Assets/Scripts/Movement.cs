503c5e6e4871f46a8ff57dd659523bf5fad6e7e6
datafedai <datafedai@gmail.com>
2025-11-14 08:53:28 -0500

    referee movement added

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+
+    InputAction moveAction;
+    [SerializeField] int speed;
+
+    void moveCharacter()
+
+    {
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        transform.Translate(moveValue * speed * Time.deltaTime, 0f);        
+    }
+
+
+
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        speed = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveCharacter();
+    }
+}
+
